b7cea60054eb32a5137437a59bd5e08bfa222ffb
lk-kai <806306842@qq.com>
2020-12-22 19:23:16 +0800

    tab下划线滑动

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
+    <div class="tab">
+      <div
+        class="tab-item"
+        ref="label"
+        v-for="(item, index) in tabsList"
+        :key="index"
+        @click="tabclick(index)"
+      >
+        <span :class="{active:item.active}">{{item.name}}</span>
+      </div>
+      <!-- 下划线 -->
+      <div class="tabs-active-bar" :style="{width:lineWidth+'px',left:lineLeftWidth+'px'}"></div>
+    </div>
     <mt-button type="primary" id="btn">primary</mt-button>
     <p>{{text | capitalize}}</p>
     <List :list="list"></List>
@@ -23,12 +36,25 @@ export default {
     $('#btn').on('click', () => {
       console.log('jquery引入成功')
     })
+    this.getWidth()
   },
   data() {
     return {
       list: [],
       text: '拉萨解放了是就',
       text1: 'aaaa',
+      tabsList: [
+        {
+          name: '首页',
+          active: true,
+        },
+        {
+          name: '评论',
+          active: false,
+        },
+      ],
+      lineWidth: 0,
+      lineLeftWidth: 0,
     }
   },
   methods: {
@@ -38,6 +64,18 @@ export default {
           this.list = res.data.data
         }
       })
+    },
+    getWidth() {
+      this.lineWidth = this.$refs.label[0].childNodes[0].offsetWidth
+      this.lineLeftWidth = this.$refs.label[0].childNodes[0].offsetLeft
+    },
+    tabclick(index) {
+      this.lineWidth = this.$refs.label[index].childNodes[0].offsetWidth
+      this.lineLeftWidth = this.$refs.label[index].childNodes[0].offsetLeft
+      this.tabsList.forEach((element) => {
+        element.active = false
+      })
+      this.tabsList[index].active = true
     },
   },
   filters: {
@@ -58,5 +96,38 @@ export default {
 * {
   margin: 0;
   padding: 0;
+}
+span {
+  margin: 0;
+  padding: 0;
+}
+.tab {
+  display: flex;
+  position: relative;
+  margin-bottom: 20px;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  font-size: 20px;
+}
+.tab-item {
+  flex: 1;
+  text-align: center;
+}
+.active {
+  color: #409eff;
+}
+.tabs-active-bar {
+  position: absolute;
+  /* left: 0; */
+  bottom: 0;
+  /* width: 56px; */
+  height: 2px;
+  /* margin-left: 4px; */
+  /* transform: translateX(0px); */
+  /* box-sizing: border-box; */
+  background-color: #409eff;
+  z-index: 1;
+  transition: all 0.3s;
 }
 </style>

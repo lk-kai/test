4853eeea91eff149ddc9db29b27160c8782540b8
lk-kai <806306842@qq.com>
2021-02-01 17:49:40 +0800

    up

--- src/view/banner/banner.vue
+++ src/view/banner/banner.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="slide-banner">
+  <div class="slide-banner" v-if="banner.length">
     <div class="banner-wrapper">
       <div class="slide-banner-wrapper" ref="slide">
         <div class="slide-banner-content">
-          <div v-for="url in urls" class="slide-page" :key="url">
-            <img :src="url" alt />
+          <div v-for="(item,index) in banner" class="slide-page" :key="index">
+            <img :src="item.picUrl" alt />
           </div>
         </div>
       </div>
       <div class="dots-wrapper">
         <span
           class="dot"
-          v-for="(item, index) in urls"
-          :key="item"
+          v-for="(item, index) in banner"
+          :key="index"
           :class="{'active': currentPageIndex ===index}"
         ></span>
       </div>
@@ -25,29 +25,38 @@
 </template>
 <script>
 import BScroll from 'better-scroll'
+import { getRecommend } from '../../api/index'
 export default {
   data() {
     return {
-      urls: [
-        'https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg',
-        'https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg',
-        'https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg',
-        'https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg',
-        'https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg',
-        'https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg',
-        'https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg',
-      ],
-      nums: 4,
+      banner: [],
       currentPageIndex: 0,
     }
   },
+  created() {
+    this._getRecommend()
+  },
   mounted() {
-    this.init()
+    // this.$nextTick(() => {
+    //   console.log(this.$refs.slide.children[0], this.banner)
+    //   //   this.init()
+    // })
   },
   beforeDestroy() {
     this.slide.destroy()
   },
   methods: {
+    _getRecommend() {
+      getRecommend().then((res) => {
+        const { code, data } = res
+        if (code === 0) {
+          this.banner = data.slider
+          this.$nextTick(() => {
+            this.init()
+          })
+        }
+      })
+    },
     init() {
       this.slide = new BScroll(this.$refs.slide, {
         scrollX: true,
@@ -58,7 +67,6 @@ export default {
         probeType: 3,
       })
       this.slide.on('scrollEnd', this._onScrollEnd)
-
       this.slide.on('slideWillChange', (page) => {
         this.currentPageIndex = page.pageX
       })
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 var appData = require('./data.json')
 var list = appData.list
 var list2 = appData.list2
+const axios = require('axios')
 module.exports = {
   devServer: {
     before: function (app) {
@@ -10,6 +11,48 @@ module.exports = {
       })
       app.get('/api/list2', function (req, res) {
         res.json({ code: 0, data: list2 })
+      })
+      app.get('/api/getTopBanner', function (req, res) {
+        const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+        const jumpPrefixMap = {
+          10002: 'https://y.qq.com/n/yqq/album/',
+          10014: 'https://y.qq.com/n/yqq/playlist/',
+          10012: 'https://y.qq.com/n/yqq/mv/v/'
+        }
+        axios.get(url, {
+          headers: {
+            referer: 'https://u.y.qq.com/',
+            host: 'u.y.qq.com'
+          },
+          params: req.query
+        }).then((response) => {
+          response = response.data
+          if (response.code === 0) {
+            const slider = []
+            const content = response.focus.data && response.focus.data.content
+            if (content) {
+              for (let i = 0; i < content.length; i++) {
+                const item = content[i]
+                const sliderItem = {}
+                const jumpPrefix = jumpPrefixMap[item.type || 10002]
+                sliderItem.id = item.id
+                sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+                sliderItem.picUrl = item.pic_info.url
+                slider.push(sliderItem)
+              }
+            }
+            res.json({
+              code: 0,
+              data: {
+                slider
+              }
+            })
+          } else {
+            res.json(response)
+          }
+        }).catch((e) => {
+          console.log(e)
+        })
       })
     }
   }

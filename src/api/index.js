211235ca486600b9576bf5a4c6a5707b370c15fe
lk-kai <806306842@qq.com>
2021-02-23 22:02:49 +0800

    配置axios

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from '../utils/http'
 const commonParams = {
   g_tk: 1928093487,
   inCharset: 'utf-8',
@@ -7,26 +7,32 @@ const commonParams = {
   format: 'jsonp'
 }
 export function getList1() {
-  return axios.get('/api/list', {}).then((res) => {
-    const { code, data } = res.data
-    // console.log(res)
-    if (code === 0) {
-      return data
-    }
-  }).catch((err) => {
-    console.log(err);
-  });
+  return axios
+    .get('/api/list', {})
+    .then((res) => {
+      const { code, data } = res.data
+      // console.log(res)
+      if (code === 0) {
+        return data
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 export function getList2() {
-  return axios.get('/api/list2', {}).then((res) => {
-    const { code, data } = res.data
-    // console.log(res)
-    if (code === 0) {
-      return data
-    }
-  }).catch((err) => {
-    console.log(err);
-  });
+  return axios
+    .get('/api/list2', {})
+    .then((res) => {
+      const { code, data } = res.data
+      // console.log(res)
+      if (code === 0) {
+        return data
+      }
+    })
+    .catch((err) => {
+      console.log(err)
+    })
 }
 export function getRecommend() {
   const url = '/api/getTopBanner'
@@ -40,7 +46,11 @@ export function getRecommend() {
     '-': 'recom' + (Math.random() + '').replace('0.', ''),
     data: {
       comm: { ct: 24 },
-      category: { method: 'get_hot_category', param: { qq: '' }, module: 'music.web_category_svr' },
+      category: {
+        method: 'get_hot_category',
+        param: { qq: '' },
+        module: 'music.web_category_svr'
+      },
       recomPlaylist: {
         method: 'get_hot_recommend',
         param: { async: 1, cmd: 2 },
@@ -51,22 +61,41 @@ export function getRecommend() {
         param: { id: 8, curPage: 1, size: 40, order: 5, titleid: 8 },
         module: 'playlist.PlayListPlazaServer'
       },
-      new_song: { module: 'newsong.NewSongServer', method: 'get_new_song_info', param: { type: 5 } },
+      new_song: {
+        module: 'newsong.NewSongServer',
+        method: 'get_new_song_info',
+        param: { type: 5 }
+      },
       new_album: {
         module: 'newalbum.NewAlbumServer',
         method: 'get_new_album_info',
         param: { area: 1, sin: 0, num: 10 }
       },
-      new_album_tag: { module: 'newalbum.NewAlbumServer', method: 'get_new_album_area', param: {} },
-      toplist: { module: 'musicToplist.ToplistInfoServer', method: 'GetAll', param: {} },
-      focus: { module: 'QQMusic.MusichallServer', method: 'GetFocus', param: {} }
+      new_album_tag: {
+        module: 'newalbum.NewAlbumServer',
+        method: 'get_new_album_area',
+        param: {}
+      },
+      toplist: {
+        module: 'musicToplist.ToplistInfoServer',
+        method: 'GetAll',
+        param: {}
+      },
+      focus: {
+        module: 'QQMusic.MusichallServer',
+        method: 'GetFocus',
+        param: {}
+      }
     }
   })
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data)
-  }).catch((e) => {
-    console.log(e);
-  })
+  return axios
+    .get(url, {
+      params: data
+    })
+    .then((res) => {
+      return Promise.resolve(res.data)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
 }

5a1d0febc31d1e96eabf923acd6385c26a4fcfbd
lk-kai <806306842@qq.com>
2023-11-13 01:38:21 +0800

    变更

---
+++ src/view/login/login1.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <el-upload
+      action="#"
+      :show-file-list="false"
+      :before-upload="beforeAvatarUpload"
+    >
+      <video v-if='videoUrl' :src='videoUrl'></video>
+      <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+    </el-upload>
+  </div>
+
+</template>
+
+<script>
+import { cutFile } from './utils'
+export default {
+  data() {
+    return {
+      videoUrl: ''
+    }
+  },
+  methods: {
+    async beforeAvatarUpload(file) {
+      console.log(file)
+      const hashList = await cutFile(file)
+      console.log(hashList)
+    }
+  }
+}
+</script>
+<style scoped>
+>>> .el-upload {
+  margin-left: 20px;
+  width: 200px;
+  height: 200px;
+  text-align: center;
+  font-size: 30px;
+  line-height: 200px;
+  background: #ccc;
+}
+</style>
